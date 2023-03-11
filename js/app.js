f0e482cb7a2a2c3d2220d9d5d12f4b999d38fc4e
engadnanhossain <engr.adnanhossain@gmail.com>
2023-03-11 13:34:28 +0600

    Added Phone Card

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log('add js file')
+const phoneLoad = async (search) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
+    const data = await res.json()
+    displayPhone(data.data)
+};
+
+const displayPhone = phones => {
+    const phonesContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.slug}</p>
+            </div>
+        </div>
+        `;
+        phonesContainer.appendChild(div)
+        console.log(phone)
+    })
+        ;
+}
+
+document.getElementById('search-button').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = ''
+    phoneLoad(searchText);
+})
+
+phoneLoad('')
